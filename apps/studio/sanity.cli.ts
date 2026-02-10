0d3ae8fd0348f8360fc1c5e2016e42fbb8be0984
shahadat-hive008 <shahadat@lemonhive.com>
2026-02-10 14:03:05 +0600

    initialize project and schema

---
+++ apps/studio/sanity.cli.ts
@@ -0,0 +1,16 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'vueoaspm',
+    dataset: 'production',
+  },
+  deployment: {
+    appId: 'l56n7vwcbpw2tt6432o2mqaa',
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
+     */
+    autoUpdates: true,
+  },
+})
